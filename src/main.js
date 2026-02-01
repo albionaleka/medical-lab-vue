f7d8b954abdeb3f40b1ce59c1b4474f7aea2786d
Albiona Leka <albioneleka@gmail.com>
2026-02-01 18:17:28 +0100

    feat(main): register pinia state management, auth layout and toast notifications

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+import authLayout from "./layout/auth.vue";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+
+createApp(App)
+  .use(pinia)
+  .use(router)
+  //.component("main-layout", mainLayout)
+  .component("auth", authLayout)
+  .use(Toast)
+  .mount("#app");
