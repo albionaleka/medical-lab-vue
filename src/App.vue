a03d4682ebcc3b6e717ef9173e4db4dfa5e8eca8
Albiona Leka <albioneleka@gmail.com>
2026-02-01 18:19:21 +0100

    feat: setup dynamic layouts and route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <component :is="$route.meta.layout ? $route.meta.layout : 'main-layout'">
+      <router-view v-slot="{ Component }">
+        <transition name="route" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </component>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import { defineComponent } from "vue";
+import AuthLayout from "./layout/auth.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    auth: AuthLayout,
+    "main-layout": AuthLayout,
+  },
+});
+</script>
+<style>
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.route-enter-active {
+  transition: all 0.3s ease-out;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
 }
 </style>
