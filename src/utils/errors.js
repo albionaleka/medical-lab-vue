b6ebef56bc348267c2acefa5eb9a0987a62d1529
Albiona Leka <albioneleka@gmail.com>
2026-02-01 18:18:09 +0100

    feat(errors): finalize AuthenticationError class structure

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,2 +1,11 @@
 export class NetworkError extends Error {}
-export class AuthenticationError extends Error {}
+export class AuthenticationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "AuthenticationError";
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, AuthenticationError);
+    }
+  }
+}
