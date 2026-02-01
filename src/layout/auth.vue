db2e1ccb54cf6fbb4d74809d79c30996e6255a5b
Albiona Leka <albioneleka@gmail.com>
2026-02-01 18:02:02 +0100

    feat(ui): add shared authentication layout

---
+++ src/layout/auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="min-h-screen w-full bg-gray-100 flex items-center justify-center p-8"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AuthLayout",
+});
+</script>
